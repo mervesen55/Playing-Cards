1cd43befd65aa55db03882f74e6a76a23e3a8cf9
ynstrz2 <merveesensoy@hotmail.com>
2023-07-29 15:07:07 +0300

    interfaces

---
+++ Assets/Scripts/UISettings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISettings : MonoBehaviour
+{
+    [SerializeField] private GameObject roomsPanel;
+    [SerializeField] private GameObject createTablePanel;
+    [SerializeField] private GameObject profilePanel;
+    [SerializeField] private GameObject tableOptionsPanel;
+
+
+    public void CreateTable()
+    {
+
+    }
+    public void TogglePlayerCount()
+    {
+
+    }
+
+
+}
