ba9ba0c2b3d97a2d1cec4805fb8dc2452d3c08e7
mervesensoy <merveesensoy@hotmail.com>
2023-07-30 12:23:08 +0300

    Bot rising

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Bot : MonoBehaviour
 {
-    [HideInInspector]
-    public bool isMyTurn;
+    [HideInInspector] public bool isMyTurn;
 
-    [HideInInspector]
-    public int WinCount;
-    [HideInInspector]
-    public int LostCount;
+    [HideInInspector] public int WinCount;
+    [HideInInspector] public int LostCount;
+
+    public Transform[] myCardPoints;
+
+    public string[] cardsInMyHand;
+
+
+    public void Decide()
+    {
+
+    }
+
+    private void PlayCard()
+    {
+
+    }
+
 }
